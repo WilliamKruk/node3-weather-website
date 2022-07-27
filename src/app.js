6d0e932ba2877092790526b1d8ae77651318f94e
William <110060774+WilliamKruk@users.noreply.github.com>
2022-07-27 11:16:07 -0400

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast.js')
 
 const app = express()
+const port = process.env.PORT || 3000
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
 
@@ -90,6 +91,6 @@ app.get('*', (req, res) => {
     renderErrorPage(res, '404: Page not found')
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Server is up on port 3000')
 })
